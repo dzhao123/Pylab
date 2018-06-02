b952cbd48c0d15491a892cf3d54eaa8284e04650
mingchuan123 <zhaomingchuantsu@gmail.com>
2018-06-02 13:41:56 -0400

    Commit message

--- MLfromScratch/random_forest/node.py
+++
@@ -1,7 +0,0 @@
-class Node(object):
-    def __init__(self,attribute):
-        self.attribute = attribute
-        self.left = None
-        self.right = None
-        #self.branch = []
-        #self.children = []
